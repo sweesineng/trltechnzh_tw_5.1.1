4acf63441121f45a10e639d1661fd7d3808e7f01
Swee Sin <sweesineng@gmail.com>
2016-06-07 19:51:35 +0800

    Linux 3.10.50

--- mm/shmem.c
+++ mm/shmem.c
@@ -483,8 +483,12 @@ static void shmem_undo_range(struct inode *inode, loff_t lstart, loff_t lend,
 			if (radix_tree_exceptional_entry(page)) {
 				if (unfalloc)
 					continue;
-				nr_swaps_freed += !shmem_free_swap(mapping,
-								index, page);
+				if (shmem_free_swap(mapping, index, page)) {
+					/* Swap was replaced by page: retry */
+					index--;
+					break;
+				}
+				nr_swaps_freed++;
 				continue;
 			}
 
